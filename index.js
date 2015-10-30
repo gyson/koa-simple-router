3b5d8793efe6e5fd39d3559fb0004df595f8ed16
gyson <eilian.yunsong@gmail.com>
2015-10-30 12:29:56 -0400

    use class

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ module.exports = setting({
 
 function setting (options) {
   router.setting = setting
+  Object.assign(router, options)
   return router
   function router (prefix, fn) {
     if (typeof prefix === 'function') {
       fn = prefix
       prefix = '/'
     }
-    let re = prefixToRegexp(prefix, options)
+    let re = prefixToRegexp(prefix, router)
     let ro = new Router(options, fn)
 
     return (ctx, next) => {
@@ -39,132 +40,6 @@ function setting (options) {
       }
       return ro._lookup(ctx, next, ctx.method, path)
     }
-  }
-}
-
-function Router (options, fn) {
-  this._options = options
-  this._map = {}
-  this._all = []
-  fn(this)
-}
-
-for (let method of methods) {
-  Router.prototype[method] = function (path, mw) {
-    if (arguments.length > 2) {
-      mw = compose(Array.from(arguments).slice(1))
-    }
-    let re = pathToRegexp(path, null, this._options)
-    return this._register(re, mw, method)
-  }
-}
-
-Router.prototype.all = function (path, mw) {
-  if (arguments.length > 2) {
-    mw = compose(Array.from(arguments).slice(1))
-  }
-  let re = pathToRegexp(path, null, this._options)
-  return this._register(re, mw)
-}
-
-Router.prototype.router = function (prefix, fn) {
-  // no prefix
-  if (typeof prefix === 'function') {
-    prefix(this)
-    return this
-  }
-  // with prefix
-  let re = prefixToRegexp(prefix, this._options)
-  let ro = new Router(this._options, fn)
-
-  return this._register(re, (ctx, next) => {
-    let raw = ctx.params[RAW]
-    let path = raw[raw.length - 1] || '/'
-    if (path[0] !== '/') {
-      path = '/' + path
-    }
-    return ro._lookup(ctx, next, ctx.method, path)
-  })
-}
-
-// with default HEAD and OPTIONS method
-Router.prototype.map = function (path, obj) {
-  let map = {}
-  for (let key of Object.keys(obj)) {
-    let mw = obj[key]
-    if (Array.isArray(mw)) {
-      mw = compose(mw)
-    }
-    if (typeof mw !== 'function') {
-      throw new Error(mw + ' is not array or function (middleware)')
-    }
-    map[key.toUpperCase()] = mw
-  }
-  if (map['GET'] && !map['HEAD']) {
-    map['HEAD'] = map['GET']
-  }
-  if (!map['OPTIONS']) {
-    map['OPTIONS'] = (ctx, next) => {
-      ctx.status = 200
-      ctx.set('Allow', allowed)
-    }
-  }
-  let allowed = Object.keys(map).join(', ')
-  return this.all(path, (ctx, next) => {
-    let method = ctx.method
-    let mw = map[method]
-    if (mw) {
-      return mw(ctx, next)
-    }
-    ctx.status = 405
-    ctx.set('Allow', allowed)
-  })
-}
-
-Router.prototype._register = function (re, mw, method) {
-  if (typeof mw !== 'function') {
-    throw new TypeError(mw + ' is not middleware function')
-  }
-  re.mw = mw
-  if (method) {
-    method = method.toUpperCase()
-    if (!this._map[method]) {
-      this._map[method] = [].concat(this._all)
-    }
-    this._map[method].push(re)
-  } else {
-    // .all
-    for (let method of Object.keys(this._map)) {
-      this._map[method].push(re)
-    }
-    this._all.push(re)
-  }
-  return this
-}
-
-Router.prototype._lookup = function (ctx, next, method, path) {
-  let group = this._map[method] || this._all
-  let index = -1
-  return dispatch(0)
-  function dispatch (i) {
-    if (i <= index) {
-      return Promise.reject(new Error('next() called multiple times'))
-    }
-    let res = null
-    while (i < group.length) {
-      res = group[i].exec(path)
-      if (res == null) {
-        i += 1
-      } else {
-        break
-      }
-    }
-    index = i
-    if (res == null) {
-      return next()
-    }
-    setParams(ctx, group[i].keys, res)
-    return tryCatch(group[i].mw, ctx, () => dispatch(i + 1))
   }
 }
 
@@ -190,18 +65,145 @@ function setParams (ctx, keys, result) {
   }
 }
 
-function tryCatch (mw, ctx, next) {
-  try {
-    return Promise.resolve(mw(ctx, next))
-  } catch (e) {
-    return Promise.reject(e)
-  }
-}
-
 function safeDecodeURIComponent (str) {
   try {
     return decodeURIComponent(str)
   } catch (e) {
     return str
+  }
+}
+
+class Router {
+  constructor (options, fn) {
+    this._options = options
+    this._map = {}
+    this._all = []
+    for (let method of methods) {
+      this[method] = function (path, mw) {
+        if (arguments.length > 2) {
+          mw = compose(Array.from(arguments).slice(1))
+        }
+        let re = pathToRegexp(path, null, this._options)
+        return this._register(re, mw, method)
+      }
+    }
+    fn(this)
+  }
+
+  all (path, mw) {
+    if (arguments.length > 2) {
+      mw = compose(Array.from(arguments).slice(1))
+    }
+    let re = pathToRegexp(path, null, this._options)
+    return this._register(re, mw)
+  }
+
+  router (prefix, fn) {
+    // no prefix
+    if (typeof prefix === 'function') {
+      prefix(this)
+      return this
+    }
+    // with prefix
+    let re = prefixToRegexp(prefix, this._options)
+    let ro = new Router(this._options, fn)
+
+    return this._register(re, (ctx, next) => {
+      let raw = ctx.params[RAW]
+      let path = raw[raw.length - 1] || '/'
+      if (path[0] !== '/') {
+        path = '/' + path
+      }
+      return ro._lookup(ctx, next, ctx.method, path)
+    })
+  }
+
+  // with default HEAD and OPTIONS method
+  map (path, obj) {
+    let map = {}
+    for (let key of Object.keys(obj)) {
+      let mw = obj[key]
+      if (Array.isArray(mw)) {
+        mw = compose(mw)
+      }
+      if (typeof mw !== 'function') {
+        throw new Error(mw + ' is not array or function (middleware)')
+      }
+      map[key.toUpperCase()] = mw
+    }
+    if (map['GET'] && !map['HEAD']) {
+      map['HEAD'] = map['GET']
+    }
+    if (!map['OPTIONS']) {
+      map['OPTIONS'] = (ctx, next) => {
+        ctx.status = 200
+        ctx.set('Allow', allowed)
+      }
+    }
+    let allowed = Object.keys(map).join(', ')
+    return this.all(path, (ctx, next) => {
+      let method = ctx.method
+      let mw = map[method]
+      if (mw) {
+        return mw(ctx, next)
+      }
+      ctx.status = 405
+      ctx.set('Allow', allowed)
+    })
+  }
+
+  _register (re, mw, method) {
+    if (typeof mw !== 'function') {
+      throw new TypeError(mw + ' is not middleware function')
+    }
+    re.mw = mw
+    if (method) {
+      method = method.toUpperCase()
+      if (!this._map[method]) {
+        this._map[method] = [].concat(this._all)
+      }
+      this._map[method].push(re)
+    } else {
+      // .all
+      for (let method of Object.keys(this._map)) {
+        this._map[method].push(re)
+      }
+      this._all.push(re)
+    }
+    return this
+  }
+
+  _lookup (ctx, next, method, path) {
+    let group = this._map[method] || this._all
+    let index = -1
+    return dispatch(0)
+    function dispatch (i) {
+      if (i <= index) {
+        return Promise.reject(new Error('next() called multiple times'))
+      }
+      let res = null
+      while (i < group.length) {
+        res = group[i].exec(path)
+        if (res == null) {
+          i += 1
+        } else {
+          break
+        }
+      }
+      index = i
+      if (res == null) {
+        return next()
+      }
+      setParams(ctx, group[i].keys, res)
+      return tryCatch(group[i].mw, ctx, () => dispatch(i + 1))
+    }
+  }
+}
+
+function tryCatch (mw, ctx, next) {
+  try {
+    return Promise.resolve(mw(ctx, next))
+  } catch (e) {
+    return Promise.reject(e)
   }
 }
